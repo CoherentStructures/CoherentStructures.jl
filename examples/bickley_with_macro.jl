fe7d53686b7405d9baed6715eaafcc7159e6038c
Nathanael Schilling <nathanaelschilling@gmx.net>
2018-02-01 14:29:58 +0100

    Minor changes to bickley_with_macro.jl

--- examples/bickley_with_macro.jl
+++ examples/bickley_with_macro.jl
@@ -1,8 +1,9 @@
-include("../src/field_from_hamiltonian.jl")
+#include("../src/field_from_hamiltonian.jl")
+using juFEMDL
 
 # after this, bickley will reference a Dictionary of functions
 # access it via the desired signature. e.g. F = bickley[:(dU, U, p, t)]
-# for the right side of the equation of variation. 
+# for the right side of the equation of variation.
 
 bickley = @makefields from stream begin
     stream = psi₀ + psi₁
@@ -24,4 +25,19 @@ bickley = @makefields from stream begin
     c₁ = 0.1446U₀ ; c₂ = 0.205U₀ ; c₃ = 0.461U₀
 
     U₀ = 62.66e-6; L₀ = 1770e-3; r₀ = 6371e-3
+end
+
+
+
+xmin = 0.0; xmax = 6.371π; ymin = -3.; ymax = 3.0
+ctx = regularDelaunayGrid((50,50),[xmin,ymin],[xmax,ymax])
+field = bickley[:(du,u,p,t)]
+cgfun = (x -> invCGTensor(field, x,[0.0,3465000], 1.e-8,tolerance=1.e-3))
+@time K = assembleStiffnessMatrix(ctx,cgfun)
+@time M = assembleMassMatrix(ctx)
+@time λ, v = eigs(K,M,which=:SM, nev= 20)
+plot_u(ctx,v[:,2])
+
+for i in 1:20
+    plot_u(ctx,v[:,i])
 end
