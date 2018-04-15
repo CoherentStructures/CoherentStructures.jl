2c2ebe44232c39e68f14afc06a1d10e2e8ddd4fb
Daniel Karrasch <daniel.karrasch@tum.de>
2018-04-15 18:48:52 +0200

    Update velocityFields.jl: distinguish in-place and not-in-place velocities, make out-of-place velocities use SVector

--- src/velocityFields.jl
+++ src/velocityFields.jl
@@ -4,8 +4,10 @@
 # rotating double gyre and Bickley jet can both be obtained from Alvaro's macro
 # this means, apart from the interpolation functions, all other functions may be removed?
 
+#TODO: Think about how to avoid code redundancy in the in-place and out-of-place versions.
+
 #The function below is taken from Oliver Junge's main_rot_gyre.jl
-function rot_double_gyre2!(du::AbstractVector{T},u::AbstractVector{T},p,t::T) where {T <: Real}
+function rot_double_gyre!(du::AbstractVector{T},u::AbstractVector{T},p,t::T) where {T <: Real}
 #function rot_double_gyre2(t::Float64,x,dx)
   st = ((t>0) & (t<1))*t^2*(3-2*t) + (t>=1)*1
   dxΨP = 2π*cos(2π*u[1])*sin(π*u[2])
@@ -16,13 +18,18 @@ function rot_double_gyre2!(du::AbstractVector{T},u::AbstractVector{T},p,t::T) wh
   du[2] = (1-st)*dxΨP + st*dxΨF
 end
 
+function rot_double_gyre(u,p,t)
+    st = ((t>0) & (t<1))*t^2*(3-2*t) + (t>=1)*1
+    dxΨP = 2π*cos(2π*u[1])*sin(π*u[2])
+    dyΨP = π*sin(2π*u[1])*cos(π*u[2])
+    dxΨF = π*cos(π*u[1])*sin(2π*u[2])
+    dyΨF = 2π*sin(π*u[1])*cos(2π*u[2])
+    du1 = - ((1-st)*dyΨP + st*dyΨF)
+    du2 = (1-st)*dxΨP + st*dxΨF
+    return SVector{2}(du1, du2)
+end
 
-# transientGyres = @ode_def tGyres begin
-#    dx = -((1-t^2*(3-2*t))*π*sin(2π*x)*cos(π*y) + t^2*(3-2*t)*2π*sin(π*x)*cos(2π*y))
-#    dy = (1-t^2*(3-2*t))*2π*cos(2π*x)*sin(π*y) + t^2*(3-2*t)*π*cos(π*x)*sin(2π*y)
-# end
-
-function transientGyresEqVari(du::AbstractVector{T},u::AbstractVector{T},p,t::T) where {T <: Real}
+function transientGyresEqVari!(du::AbstractVector{T},u::AbstractVector{T},p,t::T) where {T <: Real}
     st = ((t>0)&(t<1))*t^2*(3-2*t) + (t>1)*1
 
     # Psi_P = sin(2*pi*x)*sin(pi*y)
@@ -63,66 +70,116 @@ function transientGyresEqVari(du::AbstractVector{T},u::AbstractVector{T},p,t::T)
     du[6] = df2*u[5]+df4*u[6]
 end
 
-function bickleyJet(du::AbstractVector{T},u::AbstractVector{T},p,t::T) where {T <: Real}
-    # these parameters could be retrieved from param p
-    U = 62.66e-6
-    L = 1770e-3
-    c₂ = 1.28453e-5
-    c₃ = 2.888626e-5
-    ϵ₁ = 0.0075
-    ϵ₂ = 0.15
-    ϵ₃ = 0.3
-    k₁ = 0.31392246115209543
-    k₂ = 0.6278449223041909
-    k₃ = 0.9417673834562862
-    c₁ = 9.058543015644972e-6
+function transientGyresEqVari(u,p,t)
+    st = ((t>0)&(t<1))*t^2*(3-2*t) + (t>1)*1
+
+    # Psi_P = sin(2*pi*x)*sin(pi*y)
+    dxPsi_P = 2*pi*cos(2*pi*u[1])*sin(pi*u[2])
+    dyPsi_P = pi*sin(2*pi*u[1])*cos(pi*u[2])
+    # Psi_F = sin(pi*x)*sin(2*pi*y)
+    dxPsi_F = pi*cos(pi*u[1])*sin(2*pi*u[2])
+    dyPsi_F = 2*pi*sin(pi*u[1])*cos(2*pi*u[2])
+    # Psi = (1-st)*Psi_P + st*Psi_F;
+    dxPsi = (1-st)*dxPsi_P + st*dxPsi_F
+    dyPsi = (1-st)*dyPsi_P + st*dyPsi_F
+    # vector field
+    du1 = -dyPsi
+    du2 = dxPsi
+
+    dxdxPsi_P = -4*pi^2*sin(2*pi*u[1])*sin(pi*u[2])
+    dxdyPsi_P = 2*pi^2*cos(2*pi*u[1])*cos(pi*u[2])
+    dydyPsi_P = -pi^2*sin(2*pi*u[1])*sin(pi*u[2])
+
+    # dxPsi_F = pi*cos(pi*x)*sin(2*pi*y)
+    # dyPsi_F = 2*pi*sin(pi*x)*cos(2*pi*y)
+    dxdxPsi_F = -pi^2*sin(pi*u[1])*sin(2*pi*u[2])
+    dxdyPsi_F = 2*pi^2*cos(pi*u[1])*cos(2*pi*u[2])
+    dydyPsi_F = -4*pi^2*sin(pi*u[1])*sin(2*pi*u[2])
+
+    dxdxPsi = (1-st)*dxdxPsi_P + st*dxdxPsi_F
+    dxdyPsi = (1-st)*dxdyPsi_P + st*dxdyPsi_F
+    dydyPsi = (1-st)*dydyPsi_P + st*dydyPsi_F
+
+    # variational equation
+    df1 = -dxdyPsi
+    df2 = dxdxPsi
+    df3 = -dydyPsi
+    df4 = dxdyPsi
+    du3 = df1*u[3]+df3*u[4]
+    du4 = df2*u[3]+df4*u[4]
+    du5 = df1*u[5]+df3*u[6]
+    du6 = df2*u[5]+df4*u[6]
+    return SVector{6}(du1, du2, du3, du4, du5, du6)
+end
+
+function bickleyJet!(du::AbstractVector{T},u::AbstractVector{T},p,t::T) where {T <: Real}
+    U, L, c₁, c₂, c₃, ϵ₁, ϵ₂, ϵ₃, k₁, k₂, k₃ = p
     x = u[2]/L
     du[1] = U*sech(x)^2+(2*ϵ₁*U*cos(k₁*(u[1]-c₁*t))+2*ϵ₂*U*cos(k₂*(u[1]-c₂*t))+2*ϵ₃*U*cos(k₃*(u[1]-c₃*t)))*tanh(x)*sech(x)^2
     du[2] = -(ϵ₁*k₁*U*L*sin(k₁*(u[1]-c₁*t)) + ϵ₂*k₂*U*L*sin(k₂*(u[1]-c₂*t)) + ϵ₃*k₃*U*L*sin(k₃*(u[1]-c₃*t)))*sech(x)^2
 end
 
-function bickleyJetEqVari(du::AbstractVector{T}, u::AbstractVector{T}, p, t::T) where {T <: Real}
- # velo = bickleyJet(t,[u[1],u[2]])
- # du[1] = velo[1]
- # du[2] = velo[2]
- # gradvelo = bickleyJet(Val{:jac},t,[u[1],u[2]])
- # du[3] = gradvelo[1]*u[3]+gradvelo[3]*u[4]
- # du[4] = gradvelo[2]*u[3]+gradvelo[4]*u[4]
- # du[5] = gradvelo[1]*u[5]+gradvelo[3]*u[6]
- # du[6] = gradvelo[2]*u[5]+gradvelo[4]*u[6]
- U=62.66e-6; L=1770e-3; c₂=1.28453e-5; c₃=2.888626e-5; ϵ₁=0.0075; ϵ₂=0.15; ϵ₃=0.3
- k₁=0.31392246115209543; k₂=0.6278449223041909; k₃=0.9417673834562862; c₁=9.058543015644972e-6
- du[1] = U*sech(u[2]/L)^2+(2*ϵ₁*U*cos(k₁*(u[1]-c₁*t))+2*ϵ₂*U*cos(k₂*(u[1]-c₂*t))+2*ϵ₃*U*cos(k₃*(u[1]-c₃*t)))*tanh(u[2]/L)*sech(u[2]/L)^2
- du[2] = -(ϵ₁*k₁*U*L*sin(k₁*(u[1]-c₁*t)) + ϵ₂*k₂*U*L*sin(k₂*(u[1]-c₂*t)) + ϵ₃*k₃*U*L*sin(k₃*(u[1]-c₃*t)))*sech(u[2]/L)^2
- df1 = -(tanh(u[2]/L)*(2*U*ϵ₁*k₁*sin(k₁*(u[1] - c₁*t)) + 2*U*ϵ₂*k₂*sin(k₂*(u[1] - c₂*t))+2*U*ϵ₃*k₃*sin(k₃*(u[1] - c₃*t))))/cosh(u[2]/L)^2
-     - (tanh(u[2]/L)*(2*U*ϵ₁*k₁*sin(k₁*(u[1] - c₁*t)) + 2*U*ϵ₂*k₂*sin(k₂*(u[1] - c₂*t))+ 2*U*ϵ₃*k₃*sin(k₃*(u[1] - c₃*t))))/cosh(u[2]/L)^2
- df2 = -(L*U*ϵ₁*k₁^2*cos(k₁*(u[1] - c₁*t)) + L*U*ϵ₂*k₂^2*cos(k₂*(u[1] - c₂*t))+
-     L*U*ϵ₃*k₃^2*cos(k₃*(u[1] - c₃*t)))/cosh(u[2]/L)^2
- df3 = - ((tanh(u[2]/L).^2 - 1).*(2*U*ϵ₁*cos(k₁*(u[1] - c₁*t)) + 2*U*ϵ₂*cos(k₂*(u[1] - c₂*t))+
-    2*U*ϵ₃*cos(k₃*(u[1] - c₃*t))))/(L*cosh(u[2]/L)^2) - (2*U*sinh(u[2]/L))/(L*cosh(u[2]/L)^3)-
-    (2*sinh(u[2]/L).*tanh(u[2]/L)*(2*U*ϵ₁*cos(k₁*(u[1] - c₁*t)) + 2*U*ϵ₂*cos(k₂*(u[1] - c₂*t))+
-    2*U*ϵ₃*cos(k₃*(u[1] - c₃*t))))/(L*cosh(u[2]/L)^3)
- df4 = (2*sinh(u[2]/L)*(L*U*ϵ₁*k₁*sin(k₁*(u[1] - c₁*t)) + L*U*ϵ₂*k₂*sin(k₂*(u[1] - c₂*t))+
-    L*U*ϵ₃*k₃*sin(k₃*(u[1] - c₃*t))))/(L*cosh(u[2]/L)^3)
- du[3] = df1*u[3]+df3*u[4]
- du[4] = df2*u[3]+df4*u[4]
- du[5] = df1*u[5]+df3*u[6]
- du[6] = df2*u[5]+df4*u[6]
+function bickleyJet(u,p,t)
+    U, L, c₁, c₂, c₃, ϵ₁, ϵ₂, ϵ₃, k₁, k₂, k₃ = p
+    x = u[2]/L
+    du1 = U*sech(x)^2+(2*ϵ₁*U*cos(k₁*(u[1]-c₁*t))+2*ϵ₂*U*cos(k₂*(u[1]-c₂*t))+2*ϵ₃*U*cos(k₃*(u[1]-c₃*t)))*tanh(x)*sech(x)^2
+    du2 = -(ϵ₁*k₁*U*L*sin(k₁*(u[1]-c₁*t)) + ϵ₂*k₂*U*L*sin(k₂*(u[1]-c₂*t)) + ϵ₃*k₃*U*L*sin(k₃*(u[1]-c₃*t)))*sech(x)^2
+    return SVector{2}(du1, du2)
+end
+
+function bickleyJetEqVari!(du::AbstractVector{T},u::AbstractVector{T},p,t::T) where {T <: Real}
+    U, L, c₁, c₂, c₃, ϵ₁, ϵ₂, ϵ₃, k₁, k₂, k₃ = p
+    du1 = U*sech(u[2]/L)^2+(2*ϵ₁*U*cos(k₁*(u[1]-c₁*t))+2*ϵ₂*U*cos(k₂*(u[1]-c₂*t))+2*ϵ₃*U*cos(k₃*(u[1]-c₃*t)))*tanh(u[2]/L)*sech(u[2]/L)^2
+    du2 = -(ϵ₁*k₁*U*L*sin(k₁*(u[1]-c₁*t)) + ϵ₂*k₂*U*L*sin(k₂*(u[1]-c₂*t)) + ϵ₃*k₃*U*L*sin(k₃*(u[1]-c₃*t)))*sech(u[2]/L)^2
+    df1 = -(tanh(u[2]/L)*(2*U*ϵ₁*k₁*sin(k₁*(u[1] - c₁*t)) + 2*U*ϵ₂*k₂*sin(k₂*(u[1] - c₂*t))+2*U*ϵ₃*k₃*sin(k₃*(u[1] - c₃*t))))/cosh(u[2]/L)^2
+         - (tanh(u[2]/L)*(2*U*ϵ₁*k₁*sin(k₁*(u[1] - c₁*t)) + 2*U*ϵ₂*k₂*sin(k₂*(u[1] - c₂*t))+ 2*U*ϵ₃*k₃*sin(k₃*(u[1] - c₃*t))))/cosh(u[2]/L)^2
+    df2 = -(L*U*ϵ₁*k₁^2*cos(k₁*(u[1] - c₁*t)) + L*U*ϵ₂*k₂^2*cos(k₂*(u[1] - c₂*t))+
+         L*U*ϵ₃*k₃^2*cos(k₃*(u[1] - c₃*t)))/cosh(u[2]/L)^2
+    df3 = - ((tanh(u[2]/L).^2 - 1).*(2*U*ϵ₁*cos(k₁*(u[1] - c₁*t)) + 2*U*ϵ₂*cos(k₂*(u[1] - c₂*t))+
+        2*U*ϵ₃*cos(k₃*(u[1] - c₃*t))))/(L*cosh(u[2]/L)^2) - (2*U*sinh(u[2]/L))/(L*cosh(u[2]/L)^3)-
+        (2*sinh(u[2]/L).*tanh(u[2]/L)*(2*U*ϵ₁*cos(k₁*(u[1] - c₁*t)) + 2*U*ϵ₂*cos(k₂*(u[1] - c₂*t))+
+        2*U*ϵ₃*cos(k₃*(u[1] - c₃*t))))/(L*cosh(u[2]/L)^3)
+    df4 = (2*sinh(u[2]/L)*(L*U*ϵ₁*k₁*sin(k₁*(u[1] - c₁*t)) + L*U*ϵ₂*k₂*sin(k₂*(u[1] - c₂*t))+
+        L*U*ϵ₃*k₃*sin(k₃*(u[1] - c₃*t))))/(L*cosh(u[2]/L)^3)
+    du3 = df1*u[3]+df3*u[4]
+    du4 = df2*u[3]+df4*u[4]
+    du5 = df1*u[5]+df3*u[6]
+    du6 = df2*u[5]+df4*u[6]
 end # r₀=6371e-3
 
+function bickleyJetEqVari(u,p,t)
+    U, L, c₁, c₂, c₃, ϵ₁, ϵ₂, ϵ₃, k₁, k₂, k₃ = p
+    du1 = U*sech(u[2]/L)^2+(2*ϵ₁*U*cos(k₁*(u[1]-c₁*t))+2*ϵ₂*U*cos(k₂*(u[1]-c₂*t))+2*ϵ₃*U*cos(k₃*(u[1]-c₃*t)))*tanh(u[2]/L)*sech(u[2]/L)^2
+    du2 = -(ϵ₁*k₁*U*L*sin(k₁*(u[1]-c₁*t)) + ϵ₂*k₂*U*L*sin(k₂*(u[1]-c₂*t)) + ϵ₃*k₃*U*L*sin(k₃*(u[1]-c₃*t)))*sech(u[2]/L)^2
+    df1 = -(tanh(u[2]/L)*(2*U*ϵ₁*k₁*sin(k₁*(u[1] - c₁*t)) + 2*U*ϵ₂*k₂*sin(k₂*(u[1] - c₂*t))+2*U*ϵ₃*k₃*sin(k₃*(u[1] - c₃*t))))/cosh(u[2]/L)^2
+         - (tanh(u[2]/L)*(2*U*ϵ₁*k₁*sin(k₁*(u[1] - c₁*t)) + 2*U*ϵ₂*k₂*sin(k₂*(u[1] - c₂*t))+ 2*U*ϵ₃*k₃*sin(k₃*(u[1] - c₃*t))))/cosh(u[2]/L)^2
+    df2 = -(L*U*ϵ₁*k₁^2*cos(k₁*(u[1] - c₁*t)) + L*U*ϵ₂*k₂^2*cos(k₂*(u[1] - c₂*t))+
+         L*U*ϵ₃*k₃^2*cos(k₃*(u[1] - c₃*t)))/cosh(u[2]/L)^2
+    df3 = - ((tanh(u[2]/L).^2 - 1).*(2*U*ϵ₁*cos(k₁*(u[1] - c₁*t)) + 2*U*ϵ₂*cos(k₂*(u[1] - c₂*t))+
+        2*U*ϵ₃*cos(k₃*(u[1] - c₃*t))))/(L*cosh(u[2]/L)^2) - (2*U*sinh(u[2]/L))/(L*cosh(u[2]/L)^3)-
+        (2*sinh(u[2]/L).*tanh(u[2]/L)*(2*U*ϵ₁*cos(k₁*(u[1] - c₁*t)) + 2*U*ϵ₂*cos(k₂*(u[1] - c₂*t))+
+        2*U*ϵ₃*cos(k₃*(u[1] - c₃*t))))/(L*cosh(u[2]/L)^3)
+    df4 = (2*sinh(u[2]/L)*(L*U*ϵ₁*k₁*sin(k₁*(u[1] - c₁*t)) + L*U*ϵ₂*k₂*sin(k₂*(u[1] - c₂*t))+
+        L*U*ϵ₃*k₃*sin(k₃*(u[1] - c₃*t))))/(L*cosh(u[2]/L)^3)
+    du3 = df1*u[3]+df3*u[4]
+    du4 = df2*u[3]+df4*u[4]
+    du5 = df1*u[5]+df3*u[6]
+    du6 = df2*u[5]+df4*u[6]
+    return SVector{6}(du1, du2, du3, du4, du5, du6)
+end # r₀=6371e-3
 
 #TODO: Give variables a sensible type here
-function interpolateVF(Lon,Lat,Time, UT, VT,interpolation_type=BSpline(Cubic(Free())))
+function interpolateVF(xspan,yspan,tspan,u,v,interpolation_type=BSpline(Cubic(Free())))
     # convert arrays into linspace-form for interpolation
-    const lon = linspace(minimum(Lon),maximum(Lon),length(Lon))
-    const lat = linspace(minimum(Lat),maximum(Lat),length(Lat))
-    const time = linspace(minimum(Time),maximum(Time),length(Time))
+    const X = linspace(minimum(xspan),maximum(xspan),length(xspan))
+    const Y = linspace(minimum(yspan),maximum(yspan),length(yspan))
+    const T = linspace(minimum(tspan),maximum(tspan),length(tspan))
 
-    UI = Interpolations.interpolate(UT,interpolation_type,OnGrid())
-    UI = Interpolations.scale(UI,lon,lat,time)
+    ui = Interpolations.interpolate(u,interpolation_type,OnGrid())
+    UI = Interpolations.scale(ui,X,Y,T)
     # UE = extrapolate(UI,(Linear(),Linear(),Flat()))
-    VI = Interpolations.interpolate(VT,interpolation_type,OnGrid())
-    VI = Interpolations.scale(VI,lon,lat,time)
+    vi = Interpolations.interpolate(v,interpolation_type,OnGrid())
+    VI = Interpolations.scale(vi,X,Y,T)
     # VE = extrapolate(VI,(Linear(),Linear(),Flat()))
     return UI,VI
 end
@@ -140,42 +197,3 @@ function interpolateVFPeriodic(Lon,Lat,Time, UT, VT,interpolation_type=BSpline(L
     VE = extrapolate(VI,(Periodic(),Periodic(),Flat()))
     return UE,VE
 end
-
-
-
-
-# @everywhere function oceanVFEqVari(t::Number,u::Vector,du::Vector)
-#
-#  du[1] = UI[u[1], u[2], t]
-#  du[2] = VI[u[1], u[2], t]
-#  du[3] =
-#  du[4] =
-#  du[5] =
-#  du[6] =
-# end
-
-# @everywhere function bickleyJet(t::Float64,u::AbstractArray,du::AbstractArray)
-#   x = u[1]; y = u[2];
-#   # Parameters
-#   U = 62.66e-6
-#   # U=5.4138; %Froyland&Koltai2015
-#   L = 1770e-3
-#   c2 = 1.28453e-5 #.205*U
-#   # c2=0.2054*U; %Froyland&Koltai2015
-#   c3 = 2.888626e-5 #.461*U
-#   # c3=0.4108*U; %Froyland&Koltai2015
-#   # In Gary's paper
-#   eps1 = 0.0075
-#   eps2 = 0.15
-#   eps3 = 0.3
-#   # Cranked up a bit
-#   # eps1=.9; eps2=.5; eps3=.3;
-#   # eps1 = 0; eps2 = 0.1; eps3 = 0.3; %Froyland&Koltai2015
-#   r0 = 6371e-3
-#   k1 = 0.31392246115209543 # 2/r0
-#   k2 = 0.6278449223041909 # 4/r0
-#   k3 = 0.9417673834562862 # 6/r0
-#   c1 = 9.058543015644972e-6 # c3+((sqrt(5)-1)/2)*(k2/k1)*(c2-c3)
-#   du = [U*sech(y/L)^2+(2*eps1*U*cos(k1*(x-c1*t))+2*eps2*U*cos(k2*(x-c2*t))+2*eps3*U*cos(k3*(x-c3*t)))*tanh(y/L)*sech(y/L)^2,
-#         -(eps1*k1*U*L*sin(k1*(x-c1*t)) + eps2*k2*U*L*sin(k2*(x-c2*t)) + eps3*k3*U*L*sin(k3*(x-c3*t)))*sech(y/L)^2]
-# end
