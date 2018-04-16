cb3179347fa051c6735be3491046dff2de16f278
Nathanael Schilling <schillna@ma.tum.de>
2018-04-16 15:43:00 +0200

    Advection-diffusion with lumped mass-matrices

--- src/advection_diffusion.jl
+++ src/advection_diffusion.jl
@@ -15,16 +15,24 @@ function ADimplicitEulerStep(ctx,u,edt, Afun,q=nothing,M=nothing,K=nothing)
 end
 
 
+
+
 #Helper function for the case where we've precomputed the diffusion tensors
 function PCDiffTensors(x,index,p)
     return p[index]
  end
 
-function extendedRHS(oderhs,du,u, p,t)
-    #First 4*n_quadpoints*2 points are ODE for flow-map at quadrature points
-    #The next n points are for the solution of the AD-equation
+ function extendedRHSNonStiff(oderhs,du,u,p,t)
+     print("t = $t")
     n_quadpoints = p["n_quadpoints"]
     @views arraymap(du,u,p["p"],t,oderhs,4*n_quadpoints,2)
+end
+
+function extendedRHSStiff!(A,u,p,t)
+    #First 4*n_quadpoints*2 points are ODE for flow-map at quadrature points
+    #The next n points are for the solution of the AD-equation
+
+    n_quadpoints = p["n_quadpoints"]
     n = p["n"]
     δ = p["δ"]
     DF = Vector{Tensor{2,2}}(n_quadpoints)
@@ -34,10 +42,16 @@ function extendedRHS(oderhs,du,u, p,t)
             )
     end
     invDiffTensors = dott.(inv.(DF))
-    M = p["M"]
     ctx = p["ctx"]
     ϵ = p["ϵ"]
     K = assembleStiffnessMatrix(ctx,PCDiffTensors,invDiffTensors)
-    du[4*2*n_quadpoints+1:end] = ϵ*M\(K*u[4*2*n_quadpoints+1:end])
+    I,J,V = findnz(K)
+    M = assembleMassMatrix(ctx,lumped=true)
+    for index in 1:length(I)
+        i = I[index]
+        j = J[index]
+        A[i,j] = ϵ*V[index]/M[i,i]
+    end
     print("t = $t")
+    return
 end
