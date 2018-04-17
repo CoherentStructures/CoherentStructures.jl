05912334d675bbd93dbace994f410e364f8478d6
Daniel Karrasch <daniel.karrasch@tum.de>
2018-04-17 09:04:16 +0200

    Update OceanFlow.jl: included a comparison of different tensor calculations

--- examples/OceanFlow.jl
+++ examples/OceanFlow.jl
@@ -1,43 +1,56 @@
 #OceanFlow.jl - based on code from Daniel Karrasch
 
-using juFEMDL
-using Tensors
-using Clustering #For kmeans function
+@everywhere begin
+    using juFEMDL
+    JLD2.@load "Examples/Ocean_geostrophic_velocity.jld2" Lon Lat Time UT VT
 
-JLD2.@load "examples/Ocean_geostrophic_velocity.jld2" Lon Lat Time UT VT
+    t_initial = minimum(Time)
+    t_final = t_initial + 90
 
-t_initial = minimum(Time)
-t_final = t_initial + 90
+    LL = [-4.0,-34.0]
+    UR = [6.0,-28.0]
+    UI, VI = interpolateVF(Lon,Lat,Time,UT,VT)
+    p=(UI,VI)
+    ctx = regularDelaunayGrid((100,60),LL,UR,quadrature_order=2)
 
-LL = [-4.0,-34.0]
-UR = [6.0,-28.0]
-UI, VI = interpolateVF(Lon,Lat,Time,UT,VT)
-p=(UI,VI)
-ctx = regularDelaunayGrid((100,60),LL,UR,quadrature_order=2)
+    ctx.mass_weights = [cos(deg2rad(x[2])) for x in ctx.quadrature_points]
+    times = [t_initial,t_final]
+end
 
+####### for comparison, finally only one of the following lines should remain ####### 
+# seems like mean(pullback_diffusion_tensor) is no worse than invCGTensor, maybe even faster
+# first the not-in-place SVector versions
+@everywhere icgt(x) = invCGTensor(interp_rhs, x,times, 1.e-8,tolerance=1.e-5,p=p)
+@time As = [icgt(x) for x in ctx.quadrature_points]
+@time As = pmap(icgt,ctx.quadrature_points)
 
-ctx.mass_weights = [cos(deg2rad(x[2])) for x in ctx.quadrature_points]
-times = [t_initial,t_final]
-@time As = [
-        invCGTensor(interp_rhs, SVector{2}(x...),times, 1.e-8,tolerance=1.e-5,p=p)
-        #pullback_diffusion_tensor(interp_rhs, SVector{2}(x...),times, 1.e-8,tolerance=1.e-4,p=p)
-        for x in ctx.quadrature_points
-        ] # TODO: replace by pmap call
-#As = pmap(x -> mean(pullback_diffusion_tensor(interp_rhs, SVector{2}(x...),linspace(t_initial,t_final,91), 1.e-8,Id,tolerance=1.e-4,p=p)),ctx.quadrature_points)
-#mean_As = mean.(As)
+@everywhere mpbt(x) = mean(pullback_diffusion_tensor(interp_rhs, x,times, 1.e-8,tolerance=1.e-5,p=p))
+@time As = [mpbt(x) for x in ctx.quadrature_points]
+@time As = pmap(mpbt,ctx.quadrature_points)
+
+# now the mutating
+@everywhere icgt!(x) = invCGTensor(interp_rhs!, x,times, 1.e-8,tolerance=1.e-5,p=p)
+@time As = [icgt!(x) for x in ctx.quadrature_points]
+@time As = pmap(icgt!,ctx.quadrature_points)
+
+@everywhere mpbt!(x) = mean(pullback_diffusion_tensor(interp_rhs!, x,times, 1.e-8,tolerance=1.e-5,p=p))
+@time As = [mpbt!(x) for x in ctx.quadrature_points]
+@time As = pmap(mpbt!,ctx.quadrature_points)
+######### end of comparison
+
 mean_As = As
 function mean_Afun(x,index,p)
-        return p[1][index]
+    return p[1][index]
 end
 
 #With CG-Method
 begin
-        q = [mean_As]
-        #bdata = juFEMDL.getHomDBCS(ctx)
-        bdata = juFEMDL.boundaryData()
-        @time K2 = assembleStiffnessMatrix(ctx,mean_Afun,q,bdata=bdata)
-        @time M2 = assembleMassMatrix(ctx,bdata=bdata)
-        @time λ2, v2 = eigs(K2,M2,which=:SM,nev=12)
+    q = [mean_As]
+    #bdata = juFEMDL.getHomDBCS(ctx)
+    bdata = juFEMDL.boundaryData()
+    @time K2 = assembleStiffnessMatrix(ctx,mean_Afun,q,bdata=bdata)
+    @time M2 = assembleMassMatrix(ctx,bdata=bdata)
+    @time λ2, v2 = eigs(K2,M2,which=:SM,nev=12)
 end
 plot_real_spectrum(λ2)
 plot_u(ctx,v2[:,5],200,200,bdata=bdata)
@@ -67,11 +80,11 @@ Plots.mp4(res,"/tmp/output.mp4")
 
 #With adaptive TO method
 begin
-        ocean_flow_map = u0 -> flow(interp_rhs!,u0, [t_initial,t_final],p=(UI,VI))[end]
-        @time S = assembleStiffnessMatrix(ctx)
-        @time M = assembleMassMatrix(ctx)
-        @time S2= adaptiveTO(ctx,ocean_flow_map)
-        @time λ, v = eigs(S + S2,M,which=:SM)
+    ocean_flow_map = u0 -> flow(interp_rhs!,u0, [t_initial,t_final],p=(UI,VI))[end]
+    @time S = assembleStiffnessMatrix(ctx)
+    @time M = assembleMassMatrix(ctx)
+    @time S2= adaptiveTO(ctx,ocean_flow_map)
+    @time λ, v = eigs(S + S2,M,which=:SM)
 end
 
 plot_u(ctx,v[:,3],200,200,bdata=bdata)
