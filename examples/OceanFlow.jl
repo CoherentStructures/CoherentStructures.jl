65b624d72a8b34c20ae9b9f6abdf040b94131711
Daniel Karrasch <daniel.karrasch@tum.de>
2018-04-24 23:49:01 +0200

    Update OceanFlow.jl: nomenclature

--- examples/OceanFlow.jl
+++ examples/OceanFlow.jl
@@ -19,16 +19,16 @@
 end
 
 ####### for comparison, finally only one of the following lines should remain #######
-# seems like mean(pullback_diffusion_tensor) is no worse than invCGTensor, maybe even faster
+# seems like mean(pullback_diffusion_tensor) is no worse than mean_diff_tensor, maybe even faster
 # first the not-in-place SVector versions
-@everywhere icgt(x) = invCGTensor(interp_rhs, x,times, 1.e-8,tolerance=1.e-5,p=p)
-@time As = [icgt(x) for x in ctx.quadrature_points]
-@time As = pmap(icgt,ctx.quadrature_points)
+@everywhere mdt(x) = mean_diff_tensor(interp_rhs, x,times, 1.e-8,tolerance=1.e-5,p=p)
+@time As = [mdt(x) for x in ctx.quadrature_points]
+@time As = pmap(mdt,ctx.quadrature_points)
 
 As_shared = SharedArray{Float64}(3*length(ctx.quadrature_points))
 @time @sync @parallel for index in 1:length(ctx.quadrature_points)
     x = ctx.quadrature_points[index]
-    t1,t2,t3,t4 = icgt(x)
+    t1,t2,t3,t4 = mdt(x)
     As_shared[3*(index-1) + 1] = t1
     As_shared[3*(index-1) + 2] = t2
     As_shared[3*(index-1) + 3] = t4
@@ -47,9 +47,9 @@ end
 @time As = pmap(mpbt,ctx.quadrature_points)
 
 # now the mutating
-@everywhere icgt!(x) = invCGTensor(interp_rhs!, x,times, 1.e-8,tolerance=1.e-5,p=p)
-@time As = [icgt!(x) for x in ctx.quadrature_points]
-@time As = pmap(icgt!,ctx.quadrature_points)
+@everywhere mdt!(x) = mean_diff_tensor(interp_rhs!, x,times, 1.e-8,tolerance=1.e-5,p=p)
+@time As = [mdt!(x) for x in ctx.quadrature_points]
+@time As = pmap(mdt!,ctx.quadrature_points)
 
 @everywhere mpbt!(x) = mean(pullback_diffusion_tensor(interp_rhs!, x,times, 1.e-8,tolerance=1.e-5,p=p))
 @time As = [mpbt!(x) for x in ctx.quadrature_points]
