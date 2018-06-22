3bb2c88e3d63fe385b1d130bc77e1e4189291fb2
Daniel Karrasch <Daniel.Karrasch@gmx.de>
2018-06-23 00:23:39 +0200

    advection_diffusion.jl: set factorization as default

--- src/advection_diffusion.jl
+++ src/advection_diffusion.jl
@@ -4,11 +4,15 @@
 
 # meta function
 """
-    FEM_heatflow(velocity!, ctx, tspan, κ, decomp=false, p=nothing, bdata=boundaryData(); kwargs...)
+    FEM_heatflow(velocity!, ctx, tspan, κ, decomp=true, p=nothing, bdata=boundaryData(); kwargs...)
 
 Compute the heat flow operator for the time-dependent heat equation, which
 corresponds to the advection-diffusion equation in Lagrangian coordinates, by
 employing a spatial FEM discretization and temporal implicit-Euler time stepping.
+The `decomp` argument states whether the sparse matrices should be factorized
+for the benefit of significantly faster calculations (especially in the context
+of eigendecompositions à la diffusion coordinates), at the expense of significant
+additional memory consumption. Recommendation: always try `decomp==true` first.
 
 ## Arguments
    * `odefun!`: velocity field in inplace form, e.g.,
@@ -17,29 +21,28 @@ employing a spatial FEM discretization and temporal implicit-Euler time stepping
    * `tspan`: time span determining the temporal resolution at which the heat
      flow is to be computed internally;
    * `κ`: diffusivity constant;
+   * `decomp`: use matrix factorizations instead of sparse matrices;
    * `p`: parameter container for the vector field;
    * `kwargs`: are passed to `advect_serialized_quadpoints`.
 """
 # TODO: Restrict tspan to be of Range/linspace type
-function FEM_heatflow(odefun!, ctx, tspan, κ, decomp::Bool=false, p = nothing, bdata = boundaryData(); kwargs...)
+function FEM_heatflow(odefun!, ctx, tspan, κ, p = nothing, bdata = boundaryData(); decomp::Bool=true, kwargs...)
 
     sol = advect_serialized_quadpoints(ctx, tspan, odefun!, p; kwargs...)
-    return implicitEulerStepFamily(ctx, sol, tspan, κ; bdata=bdata)
+    return implicitEulerStepFamily(ctx, sol, tspan, κ, decomp=decomp; bdata=bdata)
 end
 
-function implicitEulerStepFamily(ctx, sol, tspan, κ, decomp::Bool=false; bdata=boundaryData())
+function implicitEulerStepFamily(ctx, sol, tspan, κ; decomp::Bool=true, bdata=boundaryData())
 
     M = assembleMassMatrix(ctx, bdata=bdata)
     n = size(M)[1]
     Δτ = step(tspan)
+    # @everywhere @eval ctx, sol, Δτ, n, M, bdata, κ, decomp = $ctx, $sol, $Δτ, $n, $M, $bdata, $κ, $decomp
     P = map(tspan[1:end-1]) do t
         K = stiffnessMatrixTimeT(ctx, sol, t, bdata=bdata)
         ΔM = decomp ? factorize(M - Δτ * κ * K) : M - Δτ * κ * K
-        # TODO: Think about in-place versions (u,v) -> u .= lin_map(v)
-        Pₜ = LinearMaps.LinearMap(
-             x -> ΔM \ (M * x),
-             x -> M * (ΔM \ x),
-             n)
+        Pₜ = LinearMaps.LinearMap((u,v) -> u .= ΔM \ v, (u,v) -> u .= ΔM \ v, n) * M
+        # Pₜ = LinearMaps.LinearMap(x -> ΔM \ (M * x), x -> M * (ΔM \ x), n)
         println("Integration time $t done")
         Pₜ
     end
