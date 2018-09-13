841b931933d96d8d919d982c50f4fb67744dadae
Nathanael Schilling <schillna@ma.tum.de>
2018-09-13 21:12:30 +0200

    More dependencies

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ ENV["GKSwstype"] = "100"
 
 using Documenter, CoherentStructures
 
-#Before running this, make sure that Distances,JLD2, Printf, Random, OrdinaryDiffEq and Clustering packages are
+#Before running this, make sure that Plots, Tensors, Distances,JLD2, Printf, Random, OrdinaryDiffEq and Clustering packages are
 #installed and added to your current environment (]add )
 makedocs(
     format=:html,
