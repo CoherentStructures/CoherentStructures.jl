f389107df501a79ced68c4ebf94bdd85086424ce
Daniel Karrasch <Daniel.Karrasch@gmx.de>
2018-11-27 12:06:56 +0100

    fem_tests.jl: simplify and use Test-functionality

--- test/fem_tests.jl
+++ test/fem_tests.jl
@@ -1,38 +1,30 @@
 #Tests for FEM-code
 
-
-using CoherentStructures
-using Arpack,Plots,SparseArrays,LinearAlgebra,Tensors,StaticArrays
+using CoherentStructures, Test
+using Arpack
 
 
 #Tests for adaptive TO method on periodic grid
-function testAdaptiveTOPeriodicGrid()
-    npoints=100*100
-    LL = [0.0,0.0]
-    UR = [2π,2π]
+@testset "adaptive TO FEM methods" begin
+    npoints = 100 * 100
+    LL = [0.0, 0.0]
+    UR = [2π, 2π]
 
-    ctx,bdata = randomDelaunayGrid(
-        npoints;on_torus=true, LL=LL,UR=UR
-        )
+    ctx, bdata = randomDelaunayGrid(npoints; on_torus=true, LL=LL, UR=UR)
     #ctx, _ = regularP2TriangularGrid((50,50),LL,UR)
     #bdata = boundaryData(ctx,PEuclidean([2π,2π]))
 
-    M = assembleMassMatrix(ctx,bdata=bdata)
-
-    S = assembleStiffnessMatrix(ctx,bdata=bdata)
-    S2= adaptiveTOCollocationStiffnessMatrix(
+    M = assembleMassMatrix(ctx, bdata=bdata)
+    S = assembleStiffnessMatrix(ctx, bdata=bdata)
+    T = adaptiveTOCollocationStiffnessMatrix(
             ctx, CoherentStructures.standardMap;
-            on_torus=true,bdata=bdata,
-            volume_preserving=false,
-            )
+            on_torus=true, bdata=bdata,
+            volume_preserving=false)
+    D = 0.5 * (S + T)
 
-    D = 0.5*(S + S2)
-    λ,v = eigs(D,M,which=:SM, nev=12)
+    λ, v = eigs(D, M, which=:SM, nev=12)
 
     #Some tests so see nothing changed
-    @assert abs(λ[2] - (-1.3)) < 1e-1
-    @assert abs(λ[2] - λ[3]) < 1e-1
-
+    @test abs(λ[2] - (-1.3)) < 1e-1
+    @test abs(λ[2] - λ[3]) < 1e-1
 end
-
-testAdaptiveTOPeriodicGrid()
