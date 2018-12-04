f21e80d737316cdf33103ef5ff005fc21cb4f190
Daniel Karrasch <Daniel.Karrasch@gmx.de>
2018-12-04 14:59:08 +0100

    test_util.jl: fix

--- test/test_util.jl
+++ test/test_util.jl
@@ -2,8 +2,8 @@ using Test, StaticArrays, OrdinaryDiffEq, Tensors, LinearAlgebra, Distributed
 using CoherentStructures
 
 @testset "periodic_diff" begin
-    @test periodic_diff(5,3,10) == 2
-    @test -periodic_diff(3,5,10) == 2
-    @test -periodic_diff(9,1,10) == 2
-    @test periodic_diff(1,9,10) == 2
+    @test CoherentStructures.periodic_diff(5,3,10) ==  2
+    @test CoherentStructures.periodic_diff(3,5,10) == -2
+    @test CoherentStructures.periodic_diff(9,1,10) == -2
+    @test CoherentStructures.periodic_diff(1,9,10) ==  2
 end
