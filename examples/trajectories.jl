2cefa0dcb8ce311b425b130f370d14eafae96df6
Daniel Karrasch <Daniel.Karrasch@gmx.de>
2019-05-22 17:50:06 -0600

    polish trajectory example

--- examples/trajectories.jl
+++ examples/trajectories.jl
@@ -17,32 +17,29 @@
 
 # We show here how to use some of these methods.
 
-# We first generate some trajectories on a set of `n' random points for the double gyre.
+# We first generate some trajectories on a set of `n` random points for the double gyre.
 
-using CoherentStructures,Tensors
+using CoherentStructures, Tensors
 
 n = 500
-tspan=range(0,stop=1.0, length=20)
+tspan = range(0, stop=1.0, length=20)
 initial_points = [Vec{2}(rand(2)) for i in 1:n]
-trajectories = [
-    flow(rot_double_gyre, initial_points[i],tspan) for i in 1:n
-    ]
+trajectories = [flow(rot_double_gyre, initial_points[i], tspan) for i in 1:n]
 
 # ## FEM adaptive TO method
 
-
 # Generate a triangulation
-# If this call fails or does not return the initial points may not be unique.
+# If this call fails or does not return, the initial points may not be unique.
+
 ctx, _ = irregularDelaunayGrid(initial_points)
 
-#Generate stiffness and mass matrices, solve eigenproblem.
+# Generate stiffness and mass matrices, solve eigenproblem.
 # If this call fails or does not return the initial points may not be unique.
-S = adaptiveTOCollocationStiffnessMatrix(ctx,(i,ts) -> trajectories[i], tspan; flow_map_mode=1
-    )
+S = adaptiveTOCollocationStiffnessMatrix(ctx, (i,ts) -> trajectories[i], tspan; flow_map_mode=1)
 M = assembleMassMatrix(ctx)
 
 using Arpack
-λ, V = eigs(S,M, which=:SM,nev=6)
+λ, V = eigs(S, M; which=:SM, nev=6)
 
 # We can plot the spectrum obtained.
 
@@ -68,8 +65,9 @@ res = iterated_kmeans(20, permutedims(V[:,2:n_partition]), n_partition)
 u = kmeansresult2LCS(res)
 u_combined = sum([u[:,i] * i for i in 1:n_partition])
 
-#We plot the result. The result looks "messy" due to the
-# fact that we used quite few trajectories
+# We plot the result. The result looks "messy" due to the fact that we used
+# only few trajectories.
+
 fig = plot_u(ctx, u_combined, 400, 400;
     color=:viridis, colorbar=:none, title="$n_partition-partition of double gyre")
 
