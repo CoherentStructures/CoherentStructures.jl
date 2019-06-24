90a3f487977fc80a7c473690cedbd10cb6dc242f
Daniel Karrasch <Daniel.Karrasch@gmx.de>
2019-06-24 08:45:07 +0200

    optimize seba

--- src/seba.jl
+++ src/seba.jl
@@ -8,42 +8,44 @@ Implements the SEBA algorithm (see https://web.maths.unsw.edu.au/~froyland/FRS18
 
 """
 function SEBA(Vin; μ=0.99/sqrt(size(Vin, 1)), tol=1e-14, maxiter=5000)
-    n, nev = size(Vin)
     V = Matrix(qr(Vin).Q)
-    S = zeros(n, nev)
+    S = zero(Vin)
     R = diagm(0 => ones(size(Vin, 2)))#TODO: Allow this as optional argument?
-    for i in 1:nev
-        if maximum(V[:,i]) - minimum(V[:,i]) < 1e-14
-            V[:,i] .+= (rand.() - 0.5) * 1e-12
+    for vi in eachcol(V)
+        if abs(-(extrema(vi)...)) < 1e-14
+            vi .+= (rand.() - 0.5) * 1e-12
         end
     end
     numiter = 1
     #R = zeros(size(R))
+    Z = similar(Vin)
+    SV = similar(R)
+    Rnew = similar(R)
     while true
-        Z = V * permutedims(R)
-        for i in 1:nev
-            S[:,i] .= (sign.(Z[:,i]) .* max.(abs.(Z[:,i]) .- μ, 0))
-            colNorm = norm(S[:,i])
+        mul!(Z, V, R')
+        for (si, zi) in zip(eachcol(S), eachcol(Z))
+            si .= (sign.(zi) .* max.(abs.(zi) .- μ, 0))
+            colNorm = norm(si)
             if colNorm != 0
-                S[:,i] ./= colNorm
+                si ./= colNorm
             end
         end
-        svdres = svd(S'V)
-        Rnew = svdres.U * svdres.V'
+        mul!(SV, S', V)
+        svdres = svd!(SV)
+        mul!(Rnew, svdres.U, svdres.V')
         if opnorm(Rnew - R) < tol
-            R = Rnew
             break
         elseif numiter > maxiter
-            throw(AssertionError("numiter > $maxiter"))
+            throw(error("numiter > $maxiter"))
         end
-        R .= Rnew
+        R = Rnew
         numiter += 1
     end
 
-    for i in 1:nev
-        S[:,i] .*= sign.(sum(S[:,i]))
-        S[:,i] ./= maximum(S[:,i])
+    for si in eachcol(S)
+        si .*= sign(sum(si))
+        si ./= maximum(si)
     end
 
-    return S[:, sortperm([-minimum(S[:,i]) for i in 1:nev])]
+    return S[:, sortperm([-minimum(si) for si in eachcol(S)])]
 end
