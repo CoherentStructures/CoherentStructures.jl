704a7c96cbe163d2f54b858596894b208a63f8f7
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-03-21 17:43:56 +0000

    build based on 8892178

---
+++ previews/PR30/generated/rot_double_gyre.jl
@@ -0,0 +1,67 @@
+using CoherentStructures, Arpack
+LL, UR = (0.0, 0.0), (1.0, 1.0)
+ctx, _ = regularTriangularGrid((50, 50), LL, UR)
+
+A = x -> mean_diff_tensor(rot_double_gyre, x, [0.0, 1.0], 1.e-10, tolerance= 1.e-4)
+K = assembleStiffnessMatrix(ctx, A)
+M = assembleMassMatrix(ctx)
+λ, v = eigs(-K, M, which=:SM);
+
+import Plots
+res = [plot_u(ctx, v[:,i], 100, 100, colorbar=:none, clim=(-3,3)) for i in 1:6];
+fig = Plots.plot(res..., margin=-10Plots.px)
+
+Plots.plot(fig)
+
+spectrum_fig = Plots.scatter(1:6, real.(λ))
+
+Plots.plot(spectrum_fig)
+
+using Clustering
+
+ctx2, _ = regularTriangularGrid((200, 200))
+v_upsampled = sample_to(v, ctx, ctx2)
+
+numclusters=2
+res = kmeans(permutedims(v_upsampled[:,2:numclusters+1]), numclusters + 1)
+u = kmeansresult2LCS(res)
+res = Plots.plot([plot_u(ctx2, u[:,i], 200, 200, color=:viridis, colorbar=:none) for i in 1:3]...)
+
+Plots.plot(res)
+
+using Distributed
+nprocs() == 1 && addprocs()
+
+@everywhere using CoherentStructures, OrdinaryDiffEq
+using StaticArrays, AxisArrays
+q = 51
+tspan = range(0., stop=1., length=q)
+nx = ny = 51
+xmin, xmax, ymin, ymax = 0.0, 1.0, 0.0, 1.0
+xspan = range(xmin, stop=xmax, length=nx)
+yspan = range(ymin, stop=ymax, length=ny)
+P = AxisArray(SVector{2}.(xspan, yspan'), xspan, yspan)
+δ = 1.e-6
+mCG_tensor = let tspan=tspan, δ=δ
+    u -> av_weighted_CG_tensor(rot_double_gyre, u, tspan, δ; tolerance=1e-6, solver=Tsit5())
+end
+
+C̅ = pmap(mCG_tensor, P; batch_size=ceil(Int, length(P)/nprocs()^2))
+p = LCSParameters(0.5)
+vortices, singularities = ellipticLCS(C̅, p; outermost=true)
+
+using Plots
+λ₁, λ₂, ξ₁, ξ₂, traceT, detT = tensor_invariants(C̅)
+fig = Plots.heatmap(xspan, yspan, permutedims(log10.(traceT));
+            aspect_ratio=1, color=:viridis, leg=true,
+            title="DBS field and transport barriers",
+            xlims=(xmin, xmax), ylims=(ymin, ymax))
+scatter!([s.coords.data for s in singularities], color=:red, label="singularities")
+scatter!([vortex.center.data for vortex in vortices], color=:yellow, label="vortex cores")
+for vortex in vortices, barrier in vortex.barriers
+    plot!(barrier.curve, w=2, label="T = $(round(barrier.p, digits=2))")
+end
+Plots.plot(fig)
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
---
+++ previews/PR30/generated/trajectories.jl
@@ -0,0 +1,139 @@
+using Distributed
+(nprocs() == 1) && addprocs()
+
+@everywhere using CoherentStructures
+using StaticArrays, Distances, Plots
+
+tspan = range(10*24*3600.0, stop=30*24*3600.0, length=41)
+m = 120; n = 41; N = m*n
+x = range(0.0, stop=20.0, length=m)
+y = range(-3.0, stop=3.0, length=n)
+f = u -> flow(bickleyJet, u, tspan, tolerance=1e-4)
+particles = vec(SVector{2}.(x, y'))
+trajectories = pmap(f, particles; batch_size=m)
+
+periods = [6.371π, Inf]
+metric = PeriodicEuclidean(periods)
+
+n_coords = 6
+
+ε = 3e-1
+kernel = gaussian(ε)
+P = sparse_diff_op(trajectories, Neighborhood(gaussiancutoff(ε/5)), kernel; metric=STmetric(metric, 1))
+λ, Ψ = diffusion_coordinates(P, n_coords)
+
+field = permutedims(reshape(Ψ[:, 2], m, n))
+fig = Plots.heatmap(x, y, field, aspect_ratio=1, color=:viridis)
+Plots.plot(fig)
+
+field = permutedims(reshape(Ψ[:, 3], m, n))
+fig = Plots.heatmap(x, y, field, aspect_ratio=1, color=:viridis)
+Plots.plot(fig)
+
+P = sparse_diff_op(trajectories, KNN(400), kernel; metric=STmetric(metric, Inf))
+λ, Ψ = diffusion_coordinates(P, n_coords)
+
+field = permutedims(reshape(Ψ[:, 2], m, n))
+fig = Plots.heatmap(x, y, field, aspect_ratio=1, color=:viridis)
+Plots.plot(fig)
+
+field = permutedims(reshape(Ψ[:, 3], m, n))
+fig = Plots.heatmap(x, y, field, aspect_ratio=1, color=:viridis)
+Plots.plot(fig)
+
+Ψ2 = SEBA(Ψ)
+
+field = permutedims(reshape(Ψ2[:, 1], m, n))
+fig = Plots.heatmap(x, y, field, aspect_ratio=1, color=:viridis)
+Plots.plot(fig)
+
+field = permutedims(reshape(Ψ2[:, 2], m, n))
+fig = Plots.heatmap(x, y, field, aspect_ratio=1, color=:viridis)
+Plots.plot(fig)
+
+import Statistics: mean
+ε = 1e-3
+kernel = gaussian(ε)
+P = sparse_diff_op_family(trajectories, Neighborhood(gaussiancutoff(ε)), kernel, mean; metric=metric)
+λ, Ψ = diffusion_coordinates(P, n_coords)
+
+field = permutedims(reshape(Ψ[:, 2], m, n))
+fig = Plots.heatmap(x, y, field, aspect_ratio=1, color=:viridis)
+Plots.plot(fig)
+
+field = permutedims(reshape(Ψ[:, 3], m, n))
+fig = Plots.heatmap(x, y, field, aspect_ratio=1, color=:viridis)
+Plots.plot(fig)
+
+ε = 0.2
+P = sparse_diff_op_family(trajectories, Neighborhood(ε), Base.one, P -> row_normalize!(min.(sum(P), 1));
+                            α=0, metric=metric)
+λ, Ψ = diffusion_coordinates(P, n_coords)
+
+field = permutedims(reshape(Ψ[:, 2], m, n))
+fig = Plots.heatmap(x, y, field, aspect_ratio=1, color=:viridis)
+Plots.plot(fig)
+
+field = permutedims(reshape(Ψ[:, 3], m, n))
+fig = Plots.heatmap(x, y, field, aspect_ratio=1, color=:viridis)
+Plots.plot(fig)
+
+ε = 1e-3
+kernel = gaussian(ε)
+P = sparse_diff_op_family(trajectories, Neighborhood(gaussiancutoff(ε)), kernel; metric=metric)
+λ, Ψ = diffusion_coordinates(P, n_coords)
+
+field = permutedims(reshape(Ψ[:, 2], m, n))
+fig = Plots.heatmap(x, y, field, aspect_ratio=1, color=:viridis)
+Plots.plot(fig)
+
+field = permutedims(reshape(Ψ[:, 3], m, n))
+fig = Plots.heatmap(x, y, field, aspect_ratio=1, color=:viridis)
+Plots.plot(fig)
+
+using CoherentStructures, StaticArrays, Tensors
+
+n = 500
+tspan = range(0, stop=1.0, length=20)
+xs, ys = rand(n), rand(n)
+particles = SVector{2}.(xs, ys)
+trajectories = [flow(rot_double_gyre, p, tspan) for p in particles]
+
+ctx, _ = irregularDelaunayGrid(Vec{2}.(particles))
+
+S = adaptiveTOCollocationStiffnessMatrix(ctx, (i, ts) -> trajectories[i], tspan; flow_map_mode=1)
+M = assembleMassMatrix(ctx)
+
+using Arpack
+λ, V = eigs(S, M; which=:SM, nev=6)
+
+using Plots
+fig = plot_real_spectrum(λ, label="")
+Plots.plot(fig)
+
+using Clustering
+function iterated_kmeans(iterations, args...)
+    best = kmeans(args...)
+    for i in 1:(iterations - 1)
+        cur = kmeans(args...)
+        if cur.totalcost < best.totalcost
+            best = cur
+        end
+    end
+    return best.assignments
+end
+
+partitions = 3
+clusters = iterated_kmeans(20, permutedims(V[:, 2:partitions]), partitions)
+
+fig = scatter(xs, ys, zcolor=clusters[ctx.node_to_dof], markersize=8, labels="")
+Plots.plot(fig)
+
+fig = plot_u(ctx, float(clusters), 400, 400;
+                color=:viridis,
+                colorbar=:none,
+                title="$partitions-partition of rotating double gyre")
+Plots.plot(fig)
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
